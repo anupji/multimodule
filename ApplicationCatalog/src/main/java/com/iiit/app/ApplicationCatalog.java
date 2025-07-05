b6f8dc15525860c52581acda17147ea6087c4578
anupji <anupping@gmail.com>
2025-07-05 15:43:46 +0530

    Application Catalog

---
+++ ApplicationCatalog/src/main/java/com/iiit/app/ApplicationCatalog.java
@@ -0,0 +1,13 @@
+package com.iiit.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationCatalog {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationCatalog.class, args);
+	}
+
+}
